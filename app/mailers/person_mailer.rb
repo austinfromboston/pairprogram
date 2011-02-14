b8325aeb46617b31a3578c158980c73a0590c24c
Austin Putman, austinfromboston <austin@rawfingertips.com>
2011-02-13 20:07:20 -0800

    add new instance var to mailer for recipient, ignore NERD_tree files

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -2,6 +2,7 @@ class PersonMailer < ActionMailer::Base
   default :from => "pairprogram@gmail.com"
   def offer_email(offer)
     @sender = offer.sender
+    @recipient = offer.recipient
     mail(:to => offer.recipient.email, :subject => "#{offer.sender.name} is offering to pair with you")
   end
 end
